d9ece4ea28aa3b455fd390e00212dc60f72b81ae
Karagiannis, Achilleas <KaragiannisAc@kotsovolos.gr>
2025-08-25 19:14:43 +0300

    minor fixes

---
+++ TravelAgency.Desktop/Converters/SeverityToBrushConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using TravelAgency.Domain.Enums;
+
+namespace TravelAgency.Desktop.Converters
+{
+    public class SeverityToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type t, object p, CultureInfo c) =>
+            value is Severity s ? s switch
+            {
+                Severity.Info => Brushes.DodgerBlue,
+                Severity.Warning => Brushes.Goldenrod,
+                Severity.Danger => Brushes.Crimson,
+                _ => Brushes.Gray
+            } : Brushes.Gray;
+
+        public object ConvertBack(object v, Type t, object p, CultureInfo c) =>
+            throw new NotSupportedException();
+    }
+}
